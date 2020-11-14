ab732a5ded62438ed42a3f21b6a9321bd079d216
Wahyu <wahyu.smallker@gmail.com>
2020-11-14 08:25:20 +0700

    wes iso ngirim ng firebase

--- Lolin/src/main.cpp
+++ Lolin/src/main.cpp
@@ -2,39 +2,46 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <DHT.h>
-#define DHTPIN  25
-#define MQPIN   36
+#define DHTPIN 25
+#define MQPIN 36
 int counter = 0;
 DHT dht(DHTPIN, DHT11);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   dht.begin();
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("LoRa Sender");
   pinMode(MQPIN, INPUT);
-  while (true)
+  // while (true)
+  // {
+  //   int mq = map(analogRead(MQPIN), 0,4095, 0, 1023);
+  //   float hum = dht.readHumidity();
+  //   float temp = dht.readTemperature();
+  //   Serial.println("MQ : "+(String)mq+"  Hum : "+(String)hum+"%  Temp : "+(String)temp);
+  //   delay(100);
+  // }
+
+  if (!LoRa.begin(915E6))
   {
-    int mq = map(analogRead(MQPIN), 0,4095, 0, 1023);
-    float hum = dht.readHumidity();
-    float temp = dht.readTemperature();
-    Serial.println("MQ : "+(String)mq+"  Hum : "+(String)hum+"%  Temp : "+(String)temp);
-    delay(100);
-  }
-  
-  if (!LoRa.begin(915E6)) {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
 }
 
-void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+void loop()
+{
+  int mq = map(analogRead(MQPIN), 0, 4095, 0, 1023);
+  int hum = dht.readHumidity();
+  int temp = dht.readTemperature();
+  char data[50];
+  sprintf(data,"{\"gas\":%d,\"temp\":%d,\"hum\":%d}",mq,temp,hum);
+  Serial.println(data);
   LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
+  LoRa.print(data);
   LoRa.endPacket();
-  counter++;
   delay(5000);
 }
--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -1,93 +1,69 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-// #include <PubSubClient.h>
+#include <ESP8266HTTPClient.h>
 #include <SPI.h>
 #include <LoRa.h>
+#include <ArduinoJson.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-const char *ssid = "KEDUBES";      // Enter your WiFi name
-const char *password = "rahasiaa"; // Enter WiFi password
-const char *mqttServer = "broker.hivemq.com";
-const int mqttPort = 1883;
-const char *mqttUser = "";
-const char *mqttPassword = "";
-WiFiClient espClient;
-// PubSubClient client(espClient);
+#define ssid "bolt"
+#define pass "11111111"
 
-int counter = 0;
+String host = "https://pe-air-quality.firebaseio.com/sensor/0/logs.json";
+String data;
+HTTPClient http;
+WiFiClientSecure client;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0);
 void onReceive(int packetSize)
 {
-  // received a packet
-  char sensor[packetSize];
-  Serial.print("Received packet '");
-  // read packet
-  for (int i = 0; i < packetSize; i++)
-  {
-    sensor[i] = (char)LoRa.read();
-  }
-  Serial.print(sensor);
-  Serial.println("'");
-  // client.publish("esppub_smallker", String(sensor).c_str());
+  
+  DynamicJsonDocument output(1024);
+  String temporary = LoRa.readString();
+  deserializeJson(output, temporary);
+  int gas = output["gas"];
+  int temp = output["temp"];
+  int hum = output["hum"];
+  String timestamp = (String)timeClient.getEpochTime();
+  Serial.println(timestamp);
+  data = "{\"co\":"+(String)gas+",\"humidity\":"+(String)hum+",\"temperature\":"+(String)temp+",\"timestamp\":"+(String)timestamp+"}";
+  Serial.println(data);
 }
 void setup()
 {
   Serial.begin(115200);
+
   if (!LoRa.begin(915E6))
   {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
-  Serial.println("Connected to the WiFi network");
-  // client.setServer(mqttServer, mqttPort);
-  // client.setCallback(callback);
-  // while (!client.connected())
-  // {
-  //   Serial.println("Connecting to MQTT...");
-  //   if (client.connect("ESP8266", mqttUser, mqttPassword))
-  //   {
-  //     Serial.println("connected");
-  //   }
-  //   else
-  //   {
-  //     Serial.print("failed with state ");
-  //     Serial.print(client.state());
-  //     delay(1000);
-  //   }
-  // }
-
+  Serial.println("IP : " + WiFi.localIP().toString());
   LoRa.onReceive(onReceive);
   LoRa.receive();
-
-  // client.subscribe("espsub");
 }
 
 void loop()
 {
-  // client.loop();
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   delay(500);
-  //   Serial.println("Connecting to WiFi..");
-  // }
-  // while (!client.connected())
-  // {
-  //   Serial.println("Connecting to MQTT...");
-  //   if (client.connect("ESP8266Client", mqttUser, mqttPassword))
-  //   {
-  //     Serial.println("connected");
-  //   }
-  //   else
-  //   {
-  //     Serial.print("failed with state ");
-  //     Serial.print(client.state());
-  //     delay(1000);
-  //   }
-  // }
-  //   client.publish("esppub_smallker", "start");
-  //   delay(5000);
+  timeClient.update();
+  if (data != "" || data != NULL)
+  {
+    client.setInsecure();
+    client.connect(host, 443);
+    http.begin(client, host);
+    int response_code = http.POST(data);
+    if (response_code == HTTP_CODE_OK)
+    {
+      Serial.println(http.getString());
+    }
+    http.end();
+  }
 }
